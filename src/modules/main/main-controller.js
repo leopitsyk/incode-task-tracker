3eb6de3db3820332602f59e0423117bff84e970d
leonid <leopitsyk@gmail.com>
2017-11-06 11:14:55 +0200

    fix errors and symplifying the variables naming

--- src/modules/main/main-controller.js
+++ src/modules/main/main-controller.js
@@ -1,6 +1,6 @@
 //Main controller Time tracking app
 'use strict';
-var mainCtrl = function($scope,localStorageService,$interval) {
+var mainCtrl = function($scope,localStorageService) {
     //Tasks: init
     //Queue
     var tasks = localStorageService.get('tasks');
@@ -10,11 +10,11 @@ var mainCtrl = function($scope,localStorageService,$interval) {
         $scope.tasks = [];
     };
     //Inwork
-    var LeotasksInwork = localStorageService.get('LeotasksInwork');
-    if(LeotasksInwork) {
-        $scope.LeotasksInwork = LeotasksInwork;
+    var tasksInwork = localStorageService.get('tasksInwork');
+    if(tasksInwork) {
+        $scope.tasksInwork = tasksInwork;
     } else {
-        $scope.LeotasksInwork = [];
+        $scope.tasksInwork = [];
     };
     //Complete
     var tasksComplete = localStorageService.get('tasksComplete');
@@ -27,19 +27,20 @@ var mainCtrl = function($scope,localStorageService,$interval) {
     $scope.$watch('tasks', function() {
         localStorageService.set('tasks', $scope.tasks);
     }, true);
-    $scope.$watch('LeotasksInwork', function() {
-        localStorageService.set('LeotasksInwork', $scope.c);
+    $scope.$watch('tasksInwork', function() {
+        localStorageService.set('tasksInwork', $scope.tasksInwork);
     }, true);
     $scope.$watch('tasksComplete', function() {
         localStorageService.set('tasksComplete', $scope.tasksComplete);
     }, true);
     //Tasks: create
-    $scope.taskCreate = function(desc){
+    $scope.taskCreate = function(desc,cost){
         var id = Math.random().toString(36);
         $scope.tasks[$scope.tasks.length] = {
             id : id,
             num : $scope.tasksCounter,
             desc : desc, 
+            cost : cost,
             turn : false,
             time : {
                 counter : 0,
@@ -48,10 +49,11 @@ var mainCtrl = function($scope,localStorageService,$interval) {
                 s : 0
             },
             total : 0,
-            status : "list"
+            status : "inqueue"
         };
         $scope.tasksCounter ++;
         $scope.desc = "";
+        $scope.cost = "";
     };
     //Tasks: delete
     $scope.taskDelete = function(item){
@@ -62,23 +64,24 @@ var mainCtrl = function($scope,localStorageService,$interval) {
         }
     };
     //Tasks: inwork
-    $scope.LeotaskInwork = function(item){
+    $scope.taskInwork = function(item){
         for(var i=0;i<$scope.tasks.length;i++){
             if($scope.tasks[i].id==item){
-                $scope.LeotasksInwork[$scope.LeotasksInwork.length] = $scope.tasks[i];
+                $scope.tasksInwork[$scope.tasksInwork.length] = $scope.tasks[i];
                 $scope.tasks.splice(i,1);
             }
         }
     };
     //Tasks: complete
     $scope.taskComplete = function(item){
-        for(var i=0;i<$scope.LeotasksInwork.length;i++){
-            if($scope.LeotasksInwork[i].id==item){
-                $scope.tasksComplete[$scope.tasksComplete.length] = $scope.LeotasksInwork[i];
-                $scope.LeotasksInwork.splice(i,1);
+        for(var i=0;i<$scope.tasksInwork.length;i++){
+            if($scope.tasksInwork[i].id==item){
+                $scope.tasksComplete[$scope.tasksComplete.length] = $scope.tasksInwork[i];
+                $scope.tasksInwork.splice(i,1);
                 location.reload();
             }
         }
     };
+    
 }
 module.exports = mainCtrl;
